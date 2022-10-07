1c579a2f26f0a427f37c3e35bd99dbc7b0377d05
曾君伟 <zengjunwei@pconline.com.cn>
2022-10-07 23:23:28 +0800

    sd

--- next.config.js
+++
@@ -1,9 +0,0 @@
-const path = require('path')
-module.exports = {
-    lessLoaderOptions: {
-        javascriptEnabled: true
-    },
-    sassOptions: {
-        includePaths: [path.join(__dirname, 'styles')],
-    },
-} 
--- pages/_app.js
+++
@@ -1,8 +0,0 @@
-// _app.js
-import React from 'react'
-
-import '../styles/global.scss'; //你的全局css样式
-
-export default function MyApp({ Component, pageProps }) {
-    return <Component {...pageProps}></Component>
-}
